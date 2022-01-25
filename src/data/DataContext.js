ee35fc6e26bdd48f5ac613c33b295adaa3e8918f
Paulosmj <psmeneguesso@gmail.com>
2022-01-25 17:30:36 -0300

    useContext #01

---
+++ src/data/DataContext.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+import React from 'react';
+
+export const data = {
+  number: 123,
+  text: 'Context API...',
+};
+
+export const DataContext = React.createContext(data);
+
+export default DataContext;
