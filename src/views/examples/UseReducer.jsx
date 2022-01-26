0524e7aae77b11e41e2ec527768f0cdb541e0fa8
Paulosmj <psmeneguesso@gmail.com>
2022-01-26 16:30:52 -0300

    useReducer #01

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,15 +1,60 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+/* eslint-disable */
 
-const UseReducer = (props) => {
-    return (
-        <div className="UseReducer">
-            <PageTitle
-                title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
-        </div>
-    )
+import React, { useReducer } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+
+const initialState = {
+  cart: [],
+  products: [],
+  use: null,
+  //foco...
+  number: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'numberAdd2':
+      return { ...state, number: state.number + 2 };
+    case 'login':
+      return { ...state, user: { name: action.payload } };
+    default:
+      return state;
+  }
 }
 
-export default UseReducer
+const UseReducer = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="UseReducer">
+      <PageTitle
+        title="Hook UseReducer"
+        subtitle="Uma outra forma de ter estado em componentes funcionais!"
+      />
+      <div className="center">
+        {state.user ? (
+          <span className="text">{state.user.name}</span>
+        ) : (
+          <span className="text">Sem Usuário</span>
+        )}
+        <span className="text">{state.number}</span>
+        <div>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: 'login', payload: 'Maria' })}
+          >
+            Login
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: 'numberAdd2' })}
+          >
+            +2
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseReducer;
